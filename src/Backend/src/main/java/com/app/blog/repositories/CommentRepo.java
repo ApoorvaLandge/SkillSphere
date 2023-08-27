be03b16e39cdad599fb641611e1817eaaa26f396
apoorva landge <apoorvalandge00@gmail.com>
2023-08-27 13:25:51 +0530

    frontend

--- src/Backend/src/main/java/com/app/blog/repositories/CommentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.app.blog.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.blog.entities.Comment;
-
-public interface CommentRepo  extends JpaRepository<Comment	, Integer> {
-
-}
